0d4348474020c9fd577002aea2d4d3e63c5ae9be
Rodrigo <rodrigo.noschang1@gmail.com>
2022-11-30 17:09:26 -0300

    robot stick by the side of the box from clicks 8 to 11. Still have to do the looking from behind the wall at some point

--- scripts/animation-script.js
+++ scripts/animation-script.js
@@ -6,25 +6,36 @@ const robotTreadmill = document.querySelector('.wheel-treadmill');
 const moveRobotToStick = (animationTime) => {
     const animationInMiliseconds = animationTime * 1000;
 
-    robot.style.animation = `moveRobotForwards ${animationTime}s ease-in-out forwards`;
-    robotTreadmill.style.animation = `moveTreadmillForwards ${animationTime}s ease-in-out forwards`;
-
+    if (joyStick.switches <= 8 || joyStick.switches >= 10) {
+        robot.style.animation = `moveRobotForwards ${animationTime}s ease-in-out forwards`;
+        robotTreadmill.style.animation = `moveTreadmillForwards ${animationTime}s ease-in-out forwards`;
+    }
     // Waits for the robot to move to the stick, so it can extend it's arm
     setTimeout(() => {
-        robotArm.style.animation = `moveArmForwards ${animationTime / 2}s ease-in-out forwards`;
-        robotHand.style.animation = `moveArmForwards ${animationTime / 2}s ease-in-out forwards`;
-
-        // Waits for the arm to fully extend, then turns the stick off
-        setTimeout(turnOff, animationInMiliseconds / 2 - 100);
+        extendRobotArm(animationTime);
     }, animationInMiliseconds)
 }
 
+const extendRobotArm = (animationTime) => {
+    const animationInMiliseconds = animationTime * 1000;
+
+    robotArm.style.animation = `moveArmForwards ${animationTime / 2}s ease-in-out forwards`;
+    robotHand.style.animation = `moveArmForwards ${animationTime / 2}s ease-in-out forwards`;
+
+    // Waits for the arm to fully extend, then turns the stick off
+    setTimeout(turnOff, animationInMiliseconds / 2 - 100);
+}
+
 const moveRobotAwayFromStick = (animationTime) => {
+    if (joyStick.switches <= 7 || joyStick.switches >= 11) {
+        robot.style.animation = `moveRobotBackwards ${animationTime}s ease-in-out forwards`;
+        robotTreadmill.style.animation = `moveTreadmillBackwards ${animationTime}s ease-in-out forwards`;
+    }
 
-    robot.style.animation = `moveRobotBackwards ${animationTime}s ease-in-out forwards`;
-    robotTreadmill.style.animation = `moveTreadmillBackwards ${animationTime}s ease-in-out forwards`;
+    retrieveRobotArm(animationTime);
+}
 
+const retrieveRobotArm = (animationTime) => {
     robotArm.style.animation = `moveArmBackwards ${animationTime / 2}s ease-in-out forwards`;
     robotHand.style.animation = `moveArmBackwards ${animationTime / 2}s ease-in-out forwards`;
 }
-
