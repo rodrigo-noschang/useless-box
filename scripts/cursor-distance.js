a77eef34730c64cdfbb3ae1164677dc104ee9928
Rodrigo <rodrigo.noschang1@gmail.com>
2022-12-02 15:43:58 -0300

    Changing proximity values on the stick

--- scripts/cursor-distance.js
+++ scripts/cursor-distance.js
@@ -1,12 +1,20 @@
 const calculateCursorDistanceFromStick = (evt) => {
-    // const xMax = 1220;
-    // const xMin = 1100; 
+    const xMax = 1220;
+    const xMin = 1100; 
+    const xAvarage = (xMax + xMin) / 2;
 
-    // const yMax = 405;
-    // const yMin = 290;
+    const yMax = 470;
+    const yMin = 380;
+    const yAvarage = (yMax + yMin) / 2;
 
-    console.log(evt.clientX, evt.clientY);
+    // console.log('Média -> ', xAvarage, yAvarage);
+    // console.log('Posicao -> ', evt.clientX, evt.clientY);
 
+    const xDistance = Math.abs(xAvarage - evt.clientX);
+    const yDistance = Math.abs(yAvarage - evt.clientY);
 
+    if (xDistance <= 150 && yDistance <= 150) {
+        console.log('Tá perto');
+    }
 
 }
